1d464db2d25e9410518c46c1af97d6204400a87d
Contratti, Alexander Michael <alexcontratti@gmail.com>
2023-04-04 17:34:30 -0400

    Animal Get Support

--- server/mongodb/actions/animal.js
+++ server/mongodb/actions/animal.js
@@ -11,3 +11,19 @@ export const createAnimal = async (newAnimalData) => {
     }
     return true
 } 
+
+export const allAnimals = async (page_size, last_id) => {
+    let data = null;
+    if (last_id == null) {
+        data = await Animal.find().limit(page_size)
+    } else {
+        data = await Animal.find({ '_id': { '$gt': last_id } }).limit(page_size);
+    }
+    //Check how pagination is supposed to be implemented? is this fine or should I do it with different pages
+    if (data[data.length - 1] == null) {
+        return [data, null];
+    } else {
+        last_id = data[data.length - 1]._id
+        return [data, last_id];
+    }
+}
