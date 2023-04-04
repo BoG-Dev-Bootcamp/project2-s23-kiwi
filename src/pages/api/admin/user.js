77758533206eae6dd971f1a7e30b3672e637b7b1
denriquez599 <denriquez599@gmail.com>
2023-04-04 16:25:08 -0400

    Renames some variables for continuity sake

--- src/pages/api/admin/user.js
+++ src/pages/api/admin/user.js
@@ -6,12 +6,12 @@ export default async function handler(req, res) {
     try {
         if (req.method == "GET") {
             await connectDB()
-            let [logs, last_id] = await allUsers(5)
-            let newLogs = null;
+            let [users, last_id] = await allUsers(5)
+            let newUsers = null;
             //Gets 5 at a time, however, unsure if to do pagination this way or with different pages 
             while (last_id != null) {
-                [newLogs, last_id] = await allUsers(5, last_id);
-                logs = [...logs, ...newLogs]
+                [newUsers, last_id] = await allUsers(5, last_id);
+                users = [...users, ...newUsers]
             }
             return res.status(200).send(logs)
         }
