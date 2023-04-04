7593c23bf95f8af670ecadf461cb7f1a25a37474
Contratti, Alexander Michael <alexcontratti@gmail.com>
2023-04-04 17:01:22 -0400

    Animal Endpoint Complete

---
+++ server/mongodb/models/animal.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const animalSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    hoursTrained: {
+        type: Number,
+        required: true
+    },
+    owner: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    //Used in Expert Level
+    profilePicture: {
+        type: String
+    }
+})
+
+//Checks if model is created, otherwise creates model
+export default mongoose.models.Animal || mongoose.model("Animal", animalSchema)
