27fcd4b823013a5321a0529857f6273ccfa76f09
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2023-04-06 18:51:50 -0400

    fix: pagination for animal and user endpoints

--- server/mongodb/actions/animal.js
+++ server/mongodb/actions/animal.js
@@ -6,18 +6,11 @@ export const createAnimal = async (newAnimalData) => {
     await newAnimal.save()
 }
 
-export const allAnimals = async (page_size, last_id) => {
-    let data = null;
-    if (last_id == null) {
-        data = await Animal.find().limit(page_size)
+export const allAnimals = async (page_size, page) => {
+    //Returns animals logs, ordered by object_id
+    if (page == 1) {
+        return await Animal.find().limit(page_size)
     } else {
-        data = await Animal.find({ '_id': { '$gt': last_id } }).limit(page_size);
-    }
-    //Check how pagination is supposed to be implemented? is this fine or should I do it with different pages
-    if (data[data.length - 1] == null) {
-        return [data, null];
-    } else {
-        last_id = data[data.length - 1]._id
-        return [data, last_id];
+        return await Animal.find().limit(page_size).skip(page_size * (page - 1))
     }
 }
--- src/pages/api/admin/training.js
+++ src/pages/api/admin/training.js
@@ -1,6 +1,5 @@
 import { allTrainingLogs } from "../../../../server/mongodb/actions/training";
 import connectDB from "../../../../server/mongodb/index"
-import Training from "../../../../server/mongodb/models/training";
 
 export default async function handler(req, res) {
     try {
