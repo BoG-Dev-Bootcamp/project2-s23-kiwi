f0191e6a555ed2dae63c64fa008538c81052c1cb
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2023-04-06 13:15:32 -0400

    feat: user login authentication

---
+++ src/pages/api/user/login.js
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt.js'
+import User from '../../../../server/mongodb/models/user'
+export default async function handler(req, res) {
+    try {
+        if (req.method == "POST") {
+            const { email, password } = req.body
+            const user = await User.findOne(email)
+            const result = await bcrypt.compare('password', user.password)
+            return result ? res.status(200).send("Logged in") : res.status(403).send("Password or email is invalid")
+        }
+    } catch (e) {
+        return res.status(400).json({ "error": e.message })
+    }
+    return res.status(500).json({ "error": "an error occurred" })
+}
