e53d7f3fc924f5d7ab201df298aef1cf71a0a6cf
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2023-04-11 22:19:32 -0400

    fix path to /

--- src/pages/api/user/verify.js
+++ src/pages/api/user/verify.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
                     secure: false,
                     sameSite: "strict",
                     maxAge: 360,
+                    path: '/'
                 });
                 res.setHeader('Set-Cookie', serialized)
                 return res.status(200).json({ "Success": "User verified" })
