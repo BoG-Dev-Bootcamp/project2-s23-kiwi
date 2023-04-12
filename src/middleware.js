cc9dac14f5ca7f5d39a2ca3c6afc3cde565a6015
Contratti, Alexander Michael <alexcontratti@gmail.com>
2023-04-11 21:31:16 -0400

    Middleware #9

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,13 @@
-export default function middleware() {
+import { NextResponse } from "next/server";
 
+export default function middleware(req) {
+    const allowedMethods = ['POST', 'GET', 'DELETE'];
+    if (allowedMethods.includes(req.method)) {
+        return NextResponse.next();
+    } else {
+        return new NextResponse(
+            JSON.stringify({ success: false, error: "Invalid request method: " + req.method }),
+            { status: 400 }
+        )
+    }
 }
