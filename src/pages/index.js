3b4c8f6f06f27ce4728083fb1f1082d78ec97a2a
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2023-04-13 00:41:52 -0400

    feat: index page descriptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,14 +16,30 @@ export default function Home() {
 
       <div class="header-row">
         <Link class="button" href="/">Admin</Link>
-        <Link class="button" href="/user/login">Login</Link> 
-        <Link href="/"><Image src="/KiwiTrain.jpg" width="200" height="200"/></Link>
+        <Link class="button" href="/user/login">Login</Link>
+        <Link href="/"><Image src="/KiwiTrain.jpg" width="200" height="200" /></Link>
         <Link class="button" href="/">Animal</Link>
         <Link class="button" href="/">Training</Link>
       </div>
       <div class="button-container">
         <div class="infoCard">
-
+          <div>
+            <p><b>Login: </b>
+              Allows you to login in or sign up to create new user if
+              information is valid. Compares and stores passwords with Bcrypt.
+              Issues JWT tokens if succesful</p>
+            <p><b>Admin: </b>
+              If issued admin access through JWT, allows you to view all users, training logs,
+              and animals. Implements pagination with object ids.</p>
+            <p><b>Animal: </b>
+              If logged in, allows users to add animals that they own. Only creates new animals if
+              information is valid.
+            </p>
+            <p><b>Training: </b>
+              If logged in, allows users to create training logs for animals that they own.
+              Only works if animal is owned by current user.
+            </p>
+          </div>
         </div>
       </div>
 
